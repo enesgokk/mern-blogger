38d1ff1ebeff203889781b73ec0cc139caaf95df
egok <macbookpro@macbookpro-MBP.home>
2022-03-18 22:48:18 +0300

    added update and delete

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,12 @@
 import express from "express"
-import {getPosts,createPost} from "../controllers/posts.js"
+import {getPosts,getSinglePost,createPost,updatePost,deletePost} from "../controllers/posts.js"
 
 const router=express.Router();
 
 router.get("/",getPosts);
+router.get("/:id",getSinglePost);
 router.post("/",createPost);
+router.patch("/:id",updatePost)
+router.delete("/:id",deletePost)
 
 export default router;
